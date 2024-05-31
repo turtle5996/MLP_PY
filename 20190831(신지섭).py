3f64a60dcd0c4cd437034fbccd599b5000a9bc26
DS_turtle <ggsjs0116@naver.com>
2024-05-31 14:28:54 +0900

    2024-05-31

--- 20190831(신지섭).py
+++ 20190831(신지섭).py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Activation, Dense
 import numpy as np
-
+    
 training_data = np.array([[0,0],[0,1],[1,0],[1,1]], "float32")
 target_data = np.array([[0],[1],[1],[0]], "float32")
 test_data_X = np.array([[0,0],[1,1],[1,0],[0,1]], "float32")
@@ -14,27 +14,27 @@ model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss='mean_squared_error', optimizer='adam', metrics=['binary_accuracy'])
 
-model.fit(training_data, target_data, epochs=200, verbose=2)
+model.fit(training_data, target_data, epochs=2000, verbose=2)
 
 model.summary()
 
 print(model.predict(training_data))
-
-print("학번 : 20190831")
-print("이름 : 신지섭")
-print("[1]학습")
-print("[2]weight 저장")
-print("[3]weight 읽기")
-print("[4]테스트")
-print("[5]종료")
-
+print("학습을 완료했습니다.")
 
 while(1):
-   sel = int(input("입력 :"))
-   match sel:
+    print("학번 : 20190831")
+    print("이름 : 신지섭")
+    print("[1]학습")
+    print("[2]weight 저장")
+    print("[3]weight 읽기")
+    print("[4]테스트")
+    print("[5]종료")
+    sel = int(input("입력 :"))
+    match sel:
        case 1:
            model.fit(training_data, target_data, epochs=200, verbose=2)
            model.summary()
+           print("학습을 완료했습니다.")
        case 2:
             model.save_weights("testweight.weights.h5")
        case 3:
@@ -45,4 +45,6 @@ while(1):
            exit()
        case _:
            print("다시 입력하세요")
+    
+
     
